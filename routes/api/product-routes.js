414241267b795cdf14c6e7f78ca10068926dd061
dossAnania <85494162+dossAnania@users.noreply.github.com>
2021-11-17 00:05:36 +1100

    started with try catch

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,15 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {} catch (err) {res.status(500).json(err);}
+  
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {} catch (err) {res.status(500).json(err);}
 });
 
 // create new product
