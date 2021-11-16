864004ae5e6b79b15df69d485ee5f6fa407cc9ca
dossAnania <85494162+dossAnania@users.noreply.github.com>
2021-11-16 22:28:32 +1100

    columns in models defined

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,34 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const Product = require('./Product');
+const Tag = require('./Tag');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Product,
+        ket: 'id'
+      },
+      tag_id: {
+        type: DataTypes.INTEGER,
+        References: {
+          model: Tag,
+          key: 'id'
+        }
+      }
+    }
   },
   {
     sequelize,
