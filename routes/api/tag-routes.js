9d7a0c5afb69fc0eab9c8f12a89d8def13109400
dossAnania <85494162+dossAnania@users.noreply.github.com>
2021-11-17 20:33:02 +1100

    tag/:id endpoint working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -22,6 +22,12 @@ router.get('/:id', async (req, res) => {
     const tagData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
+
+    if (!tagData) {
+      res.status(404).json({message: "No tags found with that ID!"})
+      return
+    }
+    res.status(200).json(tagData)
   } catch (err) {res.status(500).json(err);}
   
 });
