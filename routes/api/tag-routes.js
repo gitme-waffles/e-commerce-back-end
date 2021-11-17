cf8bd81a68c338c9d135fc829301b8141d556a91
dossAnania <85494162+dossAnania@users.noreply.github.com>
2021-11-17 22:33:41 +1100

    put tag/:id working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -37,16 +37,27 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
   // create a new tag
   try {
-    const tagData = await Tag.create(req.body);
+    const tagData = await Tag.create(req.body);    
     res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  // console.log(req);
   try {
+    const tagData = await Tag.update(req.body, {
+      title: req.body.tag_name,
+      // title: {title: req.body.tag_name},
+      where: {id: req.params.id}
+    })
+
+    if (!tagData) { 
+      res.status(404).json({message: "Tag Not Found"})
+    }
+    res.status(200).json(tagData)
   } catch (err) {
     res.status(500).json(err);
   }
