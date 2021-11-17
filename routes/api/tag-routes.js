0aadc31afba82237b00ebef17e7e11a38d09cc22
dossAnania <85494162+dossAnania@users.noreply.github.com>
2021-11-17 21:10:14 +1100

    delete tag by id working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -52,9 +52,19 @@ router.put("/:id", (req, res) => {
   }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
   try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      }
+    })
+
+    if (!tagData) {
+      res.status(404).json({message: "Tag not found"})
+    }
+    res.status(200).json(tagData)
   } catch (err) {
     res.status(500).json(err);
   }
