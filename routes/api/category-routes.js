5ee2c43030d8bc17e9637208378831cb1414d7ea
dossAnania <85494162+dossAnania@users.noreply.github.com>
2021-11-18 14:33:14 +1100

    category delete route working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -51,9 +51,18 @@ router.put("/:id", (req, res) => {
   }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
-  try {
+  try {const categoryData = await Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  if (!categoryData) {
+    res.status(404).json({ message: "ategory not found" });
+  }
+  res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
