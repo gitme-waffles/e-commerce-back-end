4d9e56aa6f6949ded6237cb671ae4a6b73644c5d
dossAnania <85494162+dossAnania@users.noreply.github.com>
2021-11-18 14:31:16 +1100

    category post route working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -33,9 +33,11 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
   try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
   }
