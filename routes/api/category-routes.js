4a7ebb491c20e691302f000875ea01f3897ecde6
dossAnania <85494162+dossAnania@users.noreply.github.com>
2021-11-18 14:20:30 +1100

    category get all route working with typo fix

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,33 +1,52 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  try {} catch (err) {res.status(500).json(err);}
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  try {} catch (err) {res.status(500).json(err);}
+  try {
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
-  try {} catch (err) {res.status(400).json(err);}
+  try {
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
-  try {} catch (err) {res.status(500).json(err);}
+  try {
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
-  try {} catch (err) {res.status(500).json(err);}
+  try {
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
